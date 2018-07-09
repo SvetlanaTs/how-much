f5a70c329853891d0f5e3a5255fb13adf72610e3
Svetlana <svetlana@gmail.com>
2018-07-09 17:25:53 +0300

    Initial Commit

---
+++ HowMuch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HowMuch
+//
+//  Created by Svetlana T on 09.07.2018.
+//  Copyright © 2018 Nemis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
