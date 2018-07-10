f2b0963449c561792bcb779bf5211806d70b95bd
Svetlana <svetlana@gmail.com>
2018-07-10 13:08:24 +0300

    Data models have been added

---
+++ HowMuch/Code/Models/Purchase.swift
@@ -0,0 +1,15 @@
+//
+//  Purchase.swift
+//  HowMuch
+//
+//  Created by Svetlana T on 10.07.2018.
+//  Copyright © 2018 Nemis. All rights reserved.
+//
+
+import Foundation
+
+struct Purchase {
+    
+    let title: String
+    let price: Double
+}
